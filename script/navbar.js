4aebab23ad13d84340f8d36017913d584656b673
Zainqalandar <zainqlandar@gmail.com>
2025-08-28 20:45:04 +0500

    A small src problem resolved

--- script/navbar.js
+++ script/navbar.js
@@ -1,26 +1,26 @@
+const messages = [
+	"WE DON'T SELL JEWELRY - WE SELL ENERGY",
+	'CRAFTED WITH PASSION, WORN WITH POWER',
+	'JEWELRY THAT SPEAKS YOUR SOUL',
+	'ENERGY IN EVERY PIECE',
+];
 
-    const messages = [
-        "WE DON'T SELL JEWELRY - WE SELL ENERGY",
-        "CRAFTED WITH PASSION, WORN WITH POWER",
-        "JEWELRY THAT SPEAKS YOUR SOUL",
-        "ENERGY IN EVERY PIECE"
-    ];
+let currentIndex = 0;
 
-    const textElement = document.querySelector(".navbar-left p");
-    const leftArrow = document.querySelector(".fa-chevron-left");
-    const rightArrow = document.querySelector(".fa-chevron-right");
+const textElement = document.querySelector('.navbar-left p');
+const leftArrow = document.querySelector('.fa-chevron-left');
+const rightArrow = document.querySelector('.fa-chevron-right');
 
-    function updateText() {
-        textElement.textContent = messages[currentIndex];
-    }
+function updateText() {
+	textElement.textContent = messages[currentIndex];
+}
 
+leftArrow.addEventListener('click', () => {
+	currentIndex = (currentIndex - 1 + messages.length) % messages.length;
+	updateText();
+});
 
-    leftArrow.addEventListener("click", () => {
-        currentIndex = (currentIndex - 1 + messages.length) % messages.length;
-        updateText();
-    });
-
-    rightArrow.addEventListener("click", () => {
-        currentIndex = (currentIndex + 1) % messages.length;
-        updateText();
-    });
+rightArrow.addEventListener('click', () => {
+	currentIndex = (currentIndex + 1) % messages.length;
+	updateText();
+});
